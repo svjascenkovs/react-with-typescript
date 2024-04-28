82e8a2c66702aade1646c086256ea8541ba93065
Aldis <svjascenkovs@gmail.com>
2024-04-28 13:30:09 +0300

    add timer start stop functionality

--- Third project Type-Safe State with Context API and useReducer/src/components/Timer.tsx
+++ Third project Type-Safe State with Context API and useReducer/src/components/Timer.tsx
@@ -1,11 +1,46 @@
 import Container from "./UI/Container.tsx";
-import { type Timer as TimerProps } from "../store/timers-context.tsx";
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from "../store/timers-context.tsx";
+import { useEffect, useRef, useState } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null); // Refs are not recreated when component function runs again (rerenders)
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
+
+  if (remainingTime <= 0 && interval.current !== null) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(function () {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime;
+          }
+          return prevTime - 50;
+        });
+      }, 50);
+      interval.current = timer;
+    } else if (!isRunning && interval.current) {
+      clearInterval(interval.current);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const formatedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formatedRemainingTime}</p>
     </Container>
   );
 }
